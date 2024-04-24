eec6e6c2af3965c3ccff22d7a0ca228bc1df2fef
jede <jede@oric.org>
2024-04-24 23:04:29 +0200

    fix

--- src/_curl_easy_setopt.s
+++ src/_curl_easy_setopt.s
@@ -12,13 +12,10 @@
 
 .proc _curl_easy_setopt
     ;;@brief CURLcode curl_easy_setopt(CURL *handle, CURLoption option, void parameter);
-
     ;;@inputA Low struct curl ptr
     ;;@inputX High struct curl ptr
     ;;@inputY CURLOPT_URL
     ;;@inputMEM_RES  parameter
-
-
     ; Save parameter
 
     sta     RES
